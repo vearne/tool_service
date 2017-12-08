7ced28fa4cb8fa584c27de9ed91ac28ce8fea189
zhuwei <wei.zhu@sinobbd.com>
2017-12-08 09:57:18 +0800

    update tool_script/tool_server.py

---
+++ tool_script/ip_tool.py
@@ -0,0 +1,20 @@
+
+def ip2int(ip):
+    item_list = ip.split('.')
+    x = 0
+    for item in item_list:
+        x = 256 * x + int(item)
+
+    return x
+
+
+def int2ip(integer):
+    item_list = []
+    while integer > 0:
+        item_list.append(str(integer % 256))
+        integer = integer / 256
+
+    item_list.reverse()
+    return '.'.join(item_list)
+
+
