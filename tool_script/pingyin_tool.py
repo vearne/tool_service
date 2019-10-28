500d628e6dd8b1a659465502568599018792c689
vearne <asdwoshiaotian@gmail.com>
2019-10-28 17:11:00 +0800

    update pingyin

--- tool_script/pingyin_tool.py
+++ tool_script/pingyin_tool.py
@@ -7,8 +7,6 @@ def get_pingyin(ss):
     :param ss: 必须是unicode类型
     :return:
     '''
-    if isinstance(ss, str):
-        ss = ss.decode('utf8')
     res = pinyin(ss, style=pypinyin.FIRST_LETTER)
     ll = []
     for item in res:
